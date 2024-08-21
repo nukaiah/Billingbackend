a863bf375f6477668e27fba0f6663f602ab89705
Yalagala Srinivas <58137408+nukaiah@users.noreply.github.com>
2024-08-21 23:05:03 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
-var port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-server.listen(port,()=>{
-    console.log("servere on 3000");
+server.listen(PORT,()=>{
+    console.log(`server on ${PORT}`);
 });
