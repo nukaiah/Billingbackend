d0ca4e7a09c94e6a3e023612d7293d576526a8b9
Yalagala Srinivas <58137408+nukaiah@users.noreply.github.com>
2024-08-28 23:18:36 +0530

    Category Controller added

--- app.js
+++ app.js
@@ -16,11 +16,7 @@ mongoose.connect(uri)
   });
 
 
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
