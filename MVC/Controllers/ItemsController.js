da90b455c903324457daac82a10860f44736fbfb
Yalagala Srinivas <58137408+nukaiah@users.noreply.github.com>
2024-08-28 23:08:52 +0530

    Category Controller added

--- MVC/Controllers/ItemsController.js
+++ MVC/Controllers/ItemsController.js
@@ -4,6 +4,8 @@ const { sendResponse, sendErrorResponse } = require('../Middlewares/Response');
 const checkAuth = require('../Middlewares/CheckAuth');
 const itemSchema = require('../Models/ItemsModels');
 const itemController = express.Router();
+const upload = require('../Middlewares/multer');
+const cloudinary = require("../Middlewares/Cloudinary");
 
 itemController.get('/getAll', checkAuth, async (req, res, next) => {
     try {
@@ -25,23 +27,27 @@ itemController.get('/getAll', checkAuth, async (req, res, next) => {
 });
 
 
-itemController.post('/add', async (req, res, next) => {
+itemController.post('/add', checkAuth,upload.fields([{ name: 'image' }]),async (req, res, next) => {
     try {
-        const itemData = new itemSchema({
-            _id: new mongoose.Types.ObjectId,
-            itemName: req.body.itemName,
-            itemPrice: req.body.itemPrice,
-            itemImage: req.body.itemImage,
-            status: "Active",
-            merchantId: req._id,
-            categoryId: req.body.categoryId,
-        });
-        const result = await itemData.save();
-        if (result) {
-            sendResponse(res, true, "Item Added", result);
-        }
-        else {
-            sendResponse(res, false, "Something went wrong try again", {});
+        if (req.files && req.files['image']) {
+            console.log(req.files['image'][0].path);
+            itemimage = (await cloudinary.uploader.upload(req.files['image'][0].path, { folder: 'Items/' })).url;
+            const itemData = new itemSchema({
+                _id: new mongoose.Types.ObjectId,
+                itemName: req.body.itemName,
+                itemPrice: req.body.itemPrice,
+                itemImage: itemimage,
+                status: "Active",
+                merchantId: req._id,
+                categoryId: req.body.categoryId,
+            });
+            const result = await itemData.save();
+            if (result) {
+                sendResponse(res, true, "Item Added", result);
+            }
+            else {
+                sendResponse(res, false, "Something went wrong try again", {});
+            }
         }
     } catch (error) {
         sendErrorResponse(res, false, "Something went wrong try again", {});
